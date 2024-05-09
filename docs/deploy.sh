060d93a64cf3d801958b1a5e4b05fa9ed11103ab
xiaofei <lixiaofei0627@gmail.com>
2024-05-09 16:39:39 +0800

    docs(docs): :memo: update deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -9,16 +9,12 @@ npm run build
 # navigate into the build output directory
 cd .vitepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f https://github.com/plus-pro-components/plus-pro-components.github.io.git master:gh-pages
 
-# rsync -av --exclude=".*" ./  root@39.103.160.178:/root/plus-pro-components
+rsync -av --exclude=".*" ./ root@39.103.160.178:/root/plus-pro-components
 
 cd -
