8b90b26a0e9583e488b3d66545e1ab63f399c1b8
xiaofei <lixiaofei0627@gmail.com>
2024-05-09 10:53:59 +0800

    test(play): :white_check_mark: update test file

--- packages/play/src/__nav.vue
+++ packages/play/src/__nav.vue
@@ -2,8 +2,8 @@
   <div class="nav">
     <router-link class="link" to="/breadcrumb">breadcrumb</router-link>
     <router-link class="link" to="/check-card">check-card</router-link>
-    <router-link class="link" to="/date-picker">date-picker</router-link>
     <router-link class="link" to="/check-card-group">check-card-group</router-link>
+    <router-link class="link" to="/date-picker">date-picker</router-link>
     <router-link class="link" to="/descriptions">descriptions</router-link>
     <router-link class="link" to="/dialog">dialog</router-link>
     <router-link class="link" to="/dialog-form">dialog-form</router-link>
@@ -15,12 +15,12 @@
     <router-link class="link" to="/input-tag">input-tag</router-link>
     <router-link class="link" to="/layout">layout</router-link>
     <router-link class="link" to="/page">page</router-link>
-    <router-link class="link" to="/pagination">pagination</router-link>
     <router-link class="link" to="/popover">popover</router-link>
+    <router-link class="link" to="/pagination">pagination</router-link>
     <router-link class="link" to="/radio">radio</router-link>
     <router-link class="link" to="/render">render</router-link>
-    <router-link class="link" to="/search">search</router-link>
     <router-link class="link" to="/sidebar">sidebar</router-link>
+    <router-link class="link" to="/search">search</router-link>
     <router-link class="link" to="/steps-form">steps-form</router-link>
     <router-link class="link" to="/table">table</router-link>
   </div>
--- packages/play/src/router/__routes.ts
+++ packages/play/src/router/__routes.ts
@@ -1,12 +1,12 @@
 export default [
   { path: '/breadcrumb', name: 'breadcrumb', component: '@/views/breadcrumb.vue' },
   { path: '/check-card', name: 'check-card', component: '@/views/check-card.vue' },
-  { path: '/date-picker', name: 'date-picker', component: '@/views/date-picker.vue' },
   {
     path: '/check-card-group',
     name: 'check-card-group',
     component: '@/views/check-card-group.vue'
   },
+  { path: '/date-picker', name: 'date-picker', component: '@/views/date-picker.vue' },
   { path: '/descriptions', name: 'descriptions', component: '@/views/descriptions.vue' },
   { path: '/dialog', name: 'dialog', component: '@/views/dialog.vue' },
   { path: '/dialog-form', name: 'dialog-form', component: '@/views/dialog-form.vue' },
@@ -18,12 +18,12 @@ export default [
   { path: '/input-tag', name: 'input-tag', component: '@/views/input-tag.vue' },
   { path: '/layout', name: 'layout', component: '@/views/layout.vue' },
   { path: '/page', name: 'page', component: '@/views/page.vue' },
-  { path: '/pagination', name: 'pagination', component: '@/views/pagination.vue' },
   { path: '/popover', name: 'popover', component: '@/views/popover.vue' },
+  { path: '/pagination', name: 'pagination', component: '@/views/pagination.vue' },
   { path: '/radio', name: 'radio', component: '@/views/radio.vue' },
   { path: '/render', name: 'render', component: '@/views/render.vue' },
-  { path: '/search', name: 'search', component: '@/views/search.vue' },
   { path: '/sidebar', name: 'sidebar', component: '@/views/sidebar.vue' },
+  { path: '/search', name: 'search', component: '@/views/search.vue' },
   { path: '/steps-form', name: 'steps-form', component: '@/views/steps-form.vue' },
   { path: '/table', name: 'table', component: '@/views/table.vue' }
 ]
