8888ce5eb411ac47f2de796529181662022566c1
maojunkun <mao-junkun[fst-tec.co.jp>
2024-05-10 09:51:13 +0900

    feat: [国际化]添加日语语言包

---
+++ packages/locale/lang/ja.ts
@@ -0,0 +1,67 @@
+export default {
+  name: 'zh-cn',
+  plus: {
+    dialog: {
+      confirmText: '確定',
+      cancelText: '取消',
+      title: 'ポップアップ'
+    },
+    datepicker: {
+      startPlaceholder: '開始時間を選択してください',
+      endPlaceholder: '終了時間を選択してください'
+    },
+    dialogForm: {
+      title: 'ポップアップフォーム'
+    },
+    drawerForm: {
+      title: 'ドゥロワーフォーム',
+      confirmText: '確定',
+      cancelText: '取消'
+    },
+    form: {
+      submitText: '送信',
+      resetText: 'リセット',
+      errorTip: 'フォームを完全に記入してから再度送信してください！'
+    },
+    field: {
+      pleaseEnter: '入力してください',
+      pleaseSelect: '選択してください'
+    },
+    popover: {
+      confirmText: '確定',
+      cancelText: '取消'
+    },
+    search: {
+      searchText: '検索',
+      resetText: 'リセット',
+      expand: '開く',
+      retract: '閉まる'
+    },
+    table: {
+      title: 'テーブル',
+      density: '密度',
+      refresh: 'リフレッシュ',
+      columnSettings: '列の設定',
+      selectAll: 'すべて選択',
+      default: 'デフォルト',
+      loose: 'ゆったり',
+      compact: 'きちんと',
+      action: 'オプション',
+      more: 'もっと見る',
+      confirmToPerformThisOperation: 'この操作を実行してもよろしいですか？',
+      prompt: 'ヒント',
+      sort: '並べ替え'
+    },
+    stepsForm: {
+      nextText: '次へ',
+      preText: '前へ',
+      submitText: '送信'
+    },
+    inputTag: {
+      placeholder: 'キーワードを入力してEnter/スペースキーを押してください'
+    },
+    header: {
+      logout: 'ログアウト'
+    }
+  }
+}
